09caa8ef0ba6c6b579d616875d4f2f922cecad29
Tobyn Collinsworth <tobyn.collinsworth@gmail.com>
2021-04-04 18:20:35 -0500

    Updated .gitignore.

---
+++ src/constants.js
@@ -0,0 +1,28 @@
+//twitch
+export const channelName = 'Dymn929';
+export const oAuthToken = '74i10g8okxa5hvecpcbrrchpwcvtx0';
+export const botName = 'AlexanderTrebot';
+
+//resource
+export const jeopardyApiUrl = 'https://jarchive-json.glitch.me/game'
+
+//program constants 
+export const startDate = '01/01/2000';
+export const timeLimit = 120000;
+export const answerPercentage = 0.75;
+
+//enums
+export const states = Object.freeze({
+    "AskedQuestion": 1,
+    "SelectedCategory": 2,
+    "Registered": 3
+});
+
+//commands
+export const commands = [
+    '!weekly',
+    '!jeopardy',
+    '!category',
+    '!whatis',
+    '!money'
+]
